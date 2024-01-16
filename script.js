e19e8cc86d58191125b7333c7ed2da07172f3285
MesfinGithinji <meshgithinji@gmail.com>
2024-01-16 23:04:51 +0300

    Using queryselector to manipulate DOM

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log("Hello World");
+/**
+ * Interacting with elements in the DOM we use the 
+ * queryselector which takes in a CSS selector and returns the first element found 
+ * 
+ * const is used to decaler variables that will not be reassigned
+ */
+const button1 = document.querySelector("#button1");
+const button2 = document.querySelector("#button2");
+const button3 = document.querySelector("#button3");
+
+const text = document.querySelector("#text");
+const xpText = document.querySelector("#xpText");
+const healthText = document.querySelector("#healthText");
+const goldText = document.querySelector("#goldText");
+const monsterStats = document.querySelector("#monsterStats");
+const monsterName = document.querySelector("#monsterName");
+const monsterHealthText = document.querySelector("#monsterHealth");
