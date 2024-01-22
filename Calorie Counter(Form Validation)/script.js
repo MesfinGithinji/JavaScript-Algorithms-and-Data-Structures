c4b41540532dcb8f37d9c7828243e6ebee965d1e
MesfinGithinji <meshgithinji@gmail.com>
2024-01-22 23:04:31 +0300

    First use of getElementById

---
+++ Calorie Counter(Form Validation)/script.js
@@ -0,0 +1,7 @@
+let calorieCounter = document.getElementById("calorie-counter");
+/**
+ * Here we will use document.getElementById rather than
+ * document.querySelector to interact with the DOM
+ * document.getElementById allows us to select elements based on their ID attributes
+ * document.querySelector(#button) allows us to select elements based on CSS style selectors
+ */
